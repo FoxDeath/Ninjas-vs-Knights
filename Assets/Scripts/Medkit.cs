27eba06f53885c809cd612067bc8ba3edd4a6408
Radu Matusa <matusaradu@gmail.com>
2020-04-06 11:03:05 +0200

    Medpack model + movement and rotation so it looks neat

--- Assets/Scripts/Medkit.cs
+++ Assets/Scripts/Medkit.cs
@@ -6,8 +6,36 @@ public class Medkit : MonoBehaviour
 {
     [SerializeField] float healAmmount = 25f;
 
+    private Vector3 movementVector = new Vector3(0f, 1f, 0f);
+    private Vector3 startingPos;
+    private float movementFactor;
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
     public float GetHealAmmount() 
     {
         return healAmmount;
+    }
+
+    void FixedUpdate()
+    {
+        Oscilate();
+    }
+
+    private void Oscilate()
+    {
+        transform.rotation *= Quaternion.Euler(0f, 1f, 0f);
+
+        float cycles = Time.time / 3f;
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
