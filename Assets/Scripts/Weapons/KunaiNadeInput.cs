b597327892857c7ea7754fae1ab16ba3707b7a9f
Radu Matusa <matusaradu@gmail.com>
2020-04-30 14:00:44 +0200

    kunai/ base
    kunai and knight model imported for testing
    
    
    more kunai
    
    
    Fixed ui bug. Kunai's shit tho
    
    
    Kunai ready? Nani?
    
    prob not tho (def not)
    finishing touches kunai 
    
    
    Small Fix

---
+++ Assets/Scripts/Weapons/KunaiNadeInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KunaiNadeInput : MonoBehaviour
+{
+    [SerializeField] GameObject kunaiPrefab;
+    [SerializeField] float throwForce = 50f;
+    [SerializeField] float maxKunai = 4;
+
+    private float currentKunai;
+    private GameObject bulletEmitter;
+    private bool threw;
+
+    void Start()
+    {
+        currentKunai = maxKunai;
+        bulletEmitter = GameObject.Find("KunaiEmitter");
+    }
+  
+    public void KunaiInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (currentKunai > 0f)
+            {
+                currentKunai--;
+                StartCoroutine(ThrowKunai());
+                threw = true;
+            }          
+        }
+    }
+
+    public IEnumerator ThrowKunai()
+    {
+        Transform mainCamera = gameObject.transform.Find("Main Camera").transform;
+        GameObject kunai = Instantiate(kunaiPrefab, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+        Rigidbody rb = kunai.GetComponent<Rigidbody>();
+        rb.AddForce(mainCamera.forward * throwForce, ForceMode.VelocityChange);        
+        yield return new WaitForSeconds(2f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !threw)
+        {
+            if (currentKunai < maxKunai)
+            {
+                currentKunai++;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
