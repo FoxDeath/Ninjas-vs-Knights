f5763d8f7aba90fb143449b307da7f1a8c701ec8
Andrei Mataoanu <amatao1999@gmail.com>
2020-05-15 15:46:43 +0200

    Crap spawner and failed ammo pickup

---
+++ Assets/Scripts/Weapons/ManageAmmo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageAmmo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Ammo"))
+        {
+            //audioManager.Play("Pickup", GetComponent<AudioSource>());
+            //RestockAmmo();
+            //Destroy(other.gameObject);
+            print("pls");
+        }
+    }
+}
