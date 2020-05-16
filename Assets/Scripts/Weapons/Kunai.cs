6c8ada8fc562c05dc1e715ddab57eb1a7c5d20ac
Tomi Faludi <tomifaludi@gmail.com>
2020-05-16 12:36:38 +0200

    Kunai doesn't blind you if you can't see it

--- Assets/Scripts/Weapons/Kunai.cs
+++ Assets/Scripts/Weapons/Kunai.cs
@@ -6,13 +6,13 @@ public class Kunai : MonoBehaviour
     public GameObject flashEffect;
     private GameObject anchor;
 
-    public EnemyAttack enemyFlash;
     private Rigidbody rigidBody;
     private Animator anim;
 
     [SerializeField] float radius = 20f;
     [SerializeField] float delay = 3f;
     [SerializeField] float damage = 5f;
+    [SerializeField] float effectAngle = 100f;
     private float countdown;
 
     private bool exploaded = false;
@@ -69,25 +69,32 @@ public class Kunai : MonoBehaviour
         }
     }
     
-public void Flash()
+    public void Flash()
     {
         Instantiate(flashEffect, transform.position, transform.rotation);
 
         //creates a bubble, which detects everything inside of its radius
         Collider[] effected = Physics.OverlapSphere(transform.position, radius);
-        foreach (Collider nearObjects in effected)
+
+        foreach (Collider nearObject in effected)
         {
-            if (nearObjects.gameObject.layer == LayerMask.NameToLayer("Player") || nearObjects.gameObject.layer == LayerMask.NameToLayer("Enemy") && !exploaded)
+            if(!exploaded && Vector3.Angle(nearObject.transform.forward, transform.position - nearObject.transform.position) < effectAngle)
             {
-                anim.SetTrigger("Flash");                
+                RaycastHit hit;
+                
+                if(Physics.Raycast(transform.position, nearObject.transform.position - transform.position, out hit) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                {
+                    anim.SetTrigger("Flash");                
+                }
+
+                if(Physics.Raycast(transform.position, nearObject.transform.position - transform.position, out hit) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                {
+                    nearObject.GetComponent<EnemyAttack>().SetFlashed(true);   
+                }
             }
-            Destroy(gameObject);           
         }
-        EnemyAttack[] enemies = (EnemyAttack[])GameObject.FindObjectsOfType(typeof(EnemyAttack));
-        foreach (EnemyAttack enemy in enemies)
-        {
-            enemy.SetFlashed(true);           
-        }
+
+        Destroy(gameObject);
     }      
 }
 
