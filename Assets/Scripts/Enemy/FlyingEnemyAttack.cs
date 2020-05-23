22a462e5ca1bc72418be40b2fcc0a3fc2362fe75
Bence Imre Kiss <kissbenceimre99@gmail.com>
2020-05-23 23:13:41 +0200

    It be flying and avoiding stuff, still has aimbot and hitscan tho

---
+++ Assets/Scripts/Enemy/FlyingEnemyAttack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyAttack : MonoBehaviour
+{
+    private Transform target;
+
+    private bool coolingDown;
+
+    [SerializeField] float range = 20f;
+    [SerializeField] float damage = 20f;
+
+    void Update()
+    {
+        target = GetComponent<FlyingEnemyMovement>().GetTarget();
+        if (LineOfSight() && InRange() && !coolingDown)
+        {
+            StartCoroutine(AttackBehaviour());   
+        }
+    }
+
+    IEnumerator AttackBehaviour()
+    {
+        coolingDown = true;
+        target.GetComponent<Health>().TakeDamage(damage);
+        yield return new WaitForSeconds(2f);
+        coolingDown = false;
+    }
+
+    bool LineOfSight()
+    {
+        RaycastHit hit;
+
+        Vector3 dir = target.position - transform.position; 
+
+        Debug.DrawRay(transform.position, dir, Color.red);
+
+        if (Physics.Raycast(transform.position, dir, out hit, range))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool InRange()
+    {
+        return Vector3.Distance(transform.position, target.position) <= range;
+    }
+}
---
+++ Assets/Scripts/Enemy/FlyingEnemyMovement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyMovement : MonoBehaviour
+{
+    private Transform target;
+
+    private Vector3 avoidDir;
+
+    private bool hitSomething;
+
+    [SerializeField] float minDistanceFromPlayer = 15f;
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float rayCastOffset = 1f;
+    [SerializeField] float detectionRange = 1f;
+    [SerializeField] float avoidSpeedMultiplyer = 5f;
+
+    public Transform GetTarget()
+    {
+        return target;
+    }
+
+    void Update()
+    {
+        SearchForNearestPlayer();
+        Pathfind();
+    }
+
+    void SearchForNearestPlayer()
+    {
+        float closestdistance = -1f;
+        foreach(var item in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            float distance = Vector3.Distance(this.transform.position, item.transform.position);
+            if(distance < closestdistance || closestdistance == -1)
+            {
+                target = item.transform;
+                closestdistance = distance;
+            }
+        } 
+    }
+
+    void Move()
+    {
+        if (Vector3.Distance(transform.position, target.position) > minDistanceFromPlayer)
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+    }
+
+    void Turn()
+    {
+        Vector3 pos = target.position + new Vector3(0f,10f,0f) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(pos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void Pathfind()
+    {
+        RaycastHit hit;
+
+        hitSomething = false;
+
+        avoidDir = Vector3.zero;
+
+        Vector3 left = transform.position - transform.right * rayCastOffset;
+        Vector3 right = transform.position + transform.right * rayCastOffset;
+        Vector3 up = transform.position + transform.up * rayCastOffset;
+        Vector3 down = transform.position - transform.up * rayCastOffset;
+
+        Debug.DrawRay(left, transform.forward * detectionRange, Color.cyan);
+        Debug.DrawRay(right, transform.forward * detectionRange, Color.cyan);
+        Debug.DrawRay(up, transform.forward * detectionRange, Color.cyan);
+        Debug.DrawRay(down, transform.forward * detectionRange, Color.cyan);
+
+        if (Physics.Raycast(left, transform.forward, out hit, detectionRange))
+        {
+            avoidDir += Vector3.right;
+            hitSomething = true;
+        }
+        else if (Physics.Raycast(right, transform.forward, out hit, detectionRange))
+        {
+            avoidDir -= Vector3.right;
+            hitSomething = true;
+        }
+        if (Physics.Raycast(up, transform.forward, out hit, detectionRange))
+        {
+            avoidDir -= Vector3.up;
+            hitSomething = true;
+        }
+        else if (Physics.Raycast(down, transform.forward, out hit, detectionRange))
+        {
+            avoidDir += Vector3.up;
+            hitSomething = true;
+        }
+        if (hitSomething)
+        {
+            Quaternion rotation = Quaternion.LookRotation(avoidDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * avoidSpeedMultiplyer * Time.deltaTime);
+        }
+        else
+        {
+            Turn();
+        }
+        Move();
+    }
+}
