2220a1552af9a07ed1dd838532bdec264efcb10b
Tomi Faludi <tomifaludi@gmail.com>
2020-06-08 15:42:25 +0200

    Added recoil to shuriken gun and camera
    Bow has knockback + fixed weapon switch bug
    
    
    All recoil works now

--- Assets/Scripts/Weapons/ShurikenGun.cs
+++ Assets/Scripts/Weapons/ShurikenGun.cs
@@ -6,11 +6,14 @@ public class ShurikenGun :  MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject bulletEmiter;
     [SerializeField] GameObject muzzleFlash;
+    
     private AudioManager audioManager;
     private UIManager uiManager;
     private NinjaUI ninjaUI;
     private Animator animator;
     private PlayerMovement playerMovement;
+    private MouseLook mouseLook;
+    private WeaponRecoil weaponRecoil;
     
     [SerializeField] float speed = 100f;
     [SerializeField] float reloadTime = 2f;
@@ -50,6 +53,8 @@ public class ShurikenGun :  MonoBehaviour
         animator = GetComponent<Animator>();
         ninjaUI = transform.parent.parent.GetComponentInChildren<NinjaUI>();
         uiManager = UIManager.GetInstance();
+        mouseLook = GetComponentInParent<MouseLook>();
+        weaponRecoil = GetComponent<WeaponRecoil>();
     }
 
     void Start()
@@ -148,7 +153,8 @@ public class ShurikenGun :  MonoBehaviour
 
                 GetComponentInParent<NetworkController>().NetworkSpawn(bullet.name, bulletEmiter.transform.position, bulletEmiter.transform.rotation,
                 (targetPoint - bulletEmiter.transform.position).normalized * speed);
-                animator.SetTrigger("Firing");
+                mouseLook.Fire(scoping);
+                weaponRecoil.Fire(scoping);
             }
             else
             {
---
+++ Assets/Scripts/Weapons/WeaponRecoil.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRecoil : MonoBehaviour
+{
+    [SerializeField] Transform recoilPosition;
+    [SerializeField] Transform rotationPoint; 
+
+    [SerializeField] float positionRecoilSpeed = 8f;
+    [SerializeField] float rotationRecoilSpeed = 8f;
+    [SerializeField] float positionReturnSpeed = 18f;
+    [SerializeField] float rotationReturnSpeed = 38f;
+
+    [SerializeField] Vector3 recoilRotation = new Vector3(10f, 5f, 7f);
+    [SerializeField] Vector3 recoilKickBack = new Vector3(0.015f, 0f, -0.2f);
+    [SerializeField] Vector3 recoilRotationAim = new Vector3(10f, 4f, 6f);
+    [SerializeField] Vector3 recoilKickBackAim = new Vector3(0.015f, 0f, -0.2f);
+    private Vector3 rotationRecoil;
+    private Vector3 positionRecoil;
+    private Vector3 rotation;
+
+    void FixedUpdate()
+    {
+        rotationRecoil = Vector3.Lerp(rotationRecoil, Vector3.zero, rotationReturnSpeed * Time.deltaTime);
+        positionRecoil = Vector3.Lerp(positionRecoil, Vector3.zero, positionReturnSpeed * Time.deltaTime);
+
+        recoilPosition.localPosition = Vector3.Slerp(recoilPosition.localPosition + new Vector3(0f, 0f, 0.1f), positionRecoil, positionRecoilSpeed * Time.fixedDeltaTime);
+        rotation = Vector3.Slerp(rotation, rotationRecoil, rotationRecoilSpeed * Time.fixedDeltaTime);
+        rotationPoint.localRotation = Quaternion.Euler(rotation);
+    }
+
+    public void Fire(bool aiming)
+    {
+        if(aiming)
+        {   
+            rotationRecoil += new Vector3(-recoilRotationAim.x, Random.Range(-recoilRotationAim.y, recoilRotationAim.y), Random.Range(-recoilRotationAim.z, recoilRotationAim.z));
+            positionRecoil += new Vector3(Random.Range(-recoilKickBackAim.x, recoilKickBackAim.x), Random.Range(-recoilKickBackAim.y, recoilKickBackAim.y), recoilKickBackAim.z);
+        }
+        else
+        {
+            rotationRecoil += new Vector3(-recoilRotation.x, Random.Range(-recoilRotation.y, recoilRotation.y), Random.Range(-recoilRotation.z, recoilRotation.z));
+            positionRecoil += new Vector3(Random.Range(-recoilKickBack.x, recoilKickBack.x), Random.Range(-recoilKickBack.y, recoilKickBack.y), recoilKickBack.z);
+        }
+    }
+}
